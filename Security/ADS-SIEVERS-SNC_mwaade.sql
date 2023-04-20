c6759cbfb1900ec5a8d624b6d7ed4283a4128b70
Waade, Marvin <mwaade@sievers-group.com>
2023-04-20 13:05:55 +0200

    initial commit

---
+++ Security/ADS-SIEVERS-SNC_mwaade.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [ADS-SIEVERS-SNC\mwaade]
+    FROM WINDOWS WITH DEFAULT_LANGUAGE = [us_english];
+
---
+++ Security/sa.sql
@@ -0,0 +1,7 @@
+﻿CREATE LOGIN [sa]
+    WITH PASSWORD = N'#mxq=~y5ipvdiUy@nyyp}OTlmsFT7_&#$!~<d$olhkpHgngh', DEFAULT_LANGUAGE = [us_english];
+
+
+GO
+ALTER LOGIN [sa] DISABLE;
+
